0c7d8ebeeb58b97800a5a1ca177dfb13fce6028e
Simon <simon.bourque@live.ca>
2017-09-06 16:33:42 -0400

    Add project files.

---
+++ COMP371/Main.cpp
@@ -0,0 +1,99 @@
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+
+#include "Types.h"
+
+GLFWwindow* initGLFW();
+void update(float32 deltaSeconds);
+void render();
+
+int main() {
+
+	// Initialize GLFW
+	GLFWwindow* window = initGLFW();
+	if (!window) {
+		// Failed to load window
+		return 1;
+	}
+
+	// Initialize glew
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Error: failed to initialize glew." << std::endl;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	// Start loop
+	uint32 frames = 0;
+	float64 counter = 0;
+	float64 delta = 0;
+	float64 currentTime = 0;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		currentTime = glfwGetTime();
+
+		update((float32)delta);
+		render();
+		
+		glfwSwapBuffers(window);
+
+		delta = glfwGetTime() - currentTime;
+
+		counter += delta;
+		if (counter >= 1) {
+			counter = 0;
+			std::cout << "FPS: " << frames << std::endl;
+			frames = 0;
+		}
+		else {
+			frames++;
+		}
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+GLFWwindow* initGLFW() {
+	if (!glfwInit()) {
+		std::cout << "Error: failed to initialize GLFW." << std::endl;
+		return nullptr;
+	}
+
+	glfwDefaultWindowHints();
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Final Project", nullptr, nullptr);
+
+	if (!window) {
+		std::cout << "Error: failed to initialize window." << std::endl;
+		glfwTerminate();
+		return nullptr;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	
+	glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int32 width, int32 height) -> void { glViewport(0, 0, width, height); });
+
+	return window;
+}
+
+void update(float32 deltaSeconds) {
+	// Update logic...
+}
+
+void render() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Render...
+}
