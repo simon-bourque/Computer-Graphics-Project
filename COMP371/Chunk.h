5b40219deec879280a7a49c41e024a4cfcd003b3
Gabbell <contact@gabrielbelanger.com>
2017-10-14 12:11:51 -0400

    Added VBOs handle to the Chunk object.

--- COMP371/Chunk.h
+++ COMP371/Chunk.h
@@ -1,15 +1,19 @@
 #pragma once
+//Standard Library
+#include <vector>
 
 //Local headers
 #include "Types.h"
 #include "Block.h"
 
 struct Chunk {
-	//Local position of the chunk in the world
+	//Local position of the chunk in the world. Equivalent to the position of the first block of the chunk
+	//At the bottom, on top left.
 	float32 xPos;
 	float32 yPos;
 	float32 zPos;
 
 	//OpenGL Buffers
-	uint32 VAO;
+	uint32 VAO;					//VAO handle for all the VBOs (blocks) in the chunk
+	std::vector<uint32> VBOs; 	//Vector of all block data
 };
