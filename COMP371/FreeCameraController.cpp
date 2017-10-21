a7915c31b522ad06afa45ac9b87d14ed87036a93
Simon Bourque <simon.bourque@live.ca>
2017-10-20 22:12:06 -0400

    #14 Add FreeCameraController class

---
+++ COMP371/FreeCameraController.cpp
@@ -0,0 +1,163 @@
+#include "FreeCameraController.h"
+
+#include "Camera.h"
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+glm::vec2 getMouseAxis();
+
+FreeCameraController::FreeCameraController(Camera* camera) : 
+	m_camera(camera),
+	m_forwardPressed(false),
+	m_backwardPressed(false),
+	m_leftPressed(false),
+	m_rightPressed(false),
+	m_upPressed(false),
+	m_downPressed(false),
+	m_rollLeftPressed(false),
+	m_rollRightPressed(false),
+	m_rightMouseButtonPressed(false) {}
+
+
+FreeCameraController::~FreeCameraController() {}
+
+void FreeCameraController::update(float32 deltaSeconds) {
+	const static float32 SPEED = 100.0f;
+	const static float32 ROTATE_SPEED = 0.01f;
+	
+	float32 dx = 0;
+	float32 dy = 0;
+	float32 dz = 0;
+	
+	if (m_forwardPressed) {
+		--dz;
+	}
+	if (m_backwardPressed) {
+		++dz;
+	}
+	if (m_leftPressed) {
+		--dx;
+	}
+	if (m_rightPressed) {
+		++dx;
+	}
+	if (m_upPressed) {
+		++dy;
+	}
+	if (m_downPressed) {
+		--dy;
+	}
+
+	glm::vec3 deltaPos(dx,dy,dz);
+	deltaPos = (glm::normalize(deltaPos) * SPEED * deltaSeconds);
+
+	if (dx != 0 || dy != 0 || dz != 0) {
+		m_camera->transform.translateLocal(deltaPos.x, deltaPos.y, deltaPos.z);
+	}
+
+	// ###############################
+	glm::vec2 mouseAxis = getMouseAxis() * ROTATE_SPEED;
+	if (m_rightMouseButtonPressed) {
+
+		if (abs(mouseAxis.x) > abs(mouseAxis.y)) {
+			mouseAxis.y = 0;
+		}
+		else {
+			mouseAxis.x = 0;
+		}
+
+		m_camera->transform.rotateLocal(mouseAxis.y, 0, 0);
+		m_camera->transform.rotate(0, -mouseAxis.x, 0);
+	}
+
+	float32 roll = 0;
+	if (m_rollLeftPressed) {
+		++roll;
+	}
+	if (m_rollRightPressed) {
+		--roll;
+	}
+
+	if (roll != 0) {
+		//m_camera->transform.rotateLocal(0, 0, roll * ROTATE_SPEED * 100 * deltaSeconds);
+	}
+}
+
+void FreeCameraController::onKey(int32 key, int32 action) {
+	if (key == GLFW_KEY_W) {
+		if (action == GLFW_PRESS) {
+			m_forwardPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_forwardPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_S) {
+		if (action == GLFW_PRESS) {
+			m_backwardPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_backwardPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_A) {
+		if (action == GLFW_PRESS) {
+			m_leftPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_leftPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_D) {
+		if (action == GLFW_PRESS) {
+			m_rightPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_rightPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_SPACE) {
+		if (action == GLFW_PRESS) {
+			m_upPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_upPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_LEFT_CONTROL) {
+		if (action == GLFW_PRESS) {
+			m_downPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_downPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_Q) {
+		if (action == GLFW_PRESS) {
+			m_rollLeftPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_rollLeftPressed = false;
+		}
+	}
+	if (key == GLFW_KEY_E) {
+		if (action == GLFW_PRESS) {
+			m_rollRightPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_rollRightPressed = false;
+		}
+	}
+}
+
+void FreeCameraController::onMouseButton(int32 button, int32 action) {
+	if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+		if (action == GLFW_PRESS) {
+			m_rightMouseButtonPressed = true;
+		}
+		else if (action == GLFW_RELEASE) {
+			m_rightMouseButtonPressed = false;
+		}
+	}
+}
--- COMP371/Main.cpp
+++ COMP371/Main.cpp
@@ -24,6 +24,8 @@
 
 #include <vector>
 
+#include "FreeCameraController.h"
+
 GLFWwindow* initGLFW();
 void update(float32 deltaSeconds);
 void render();
@@ -39,13 +41,15 @@ glm::vec3 playerPosition(0.0f,0.0f,0.0f);
 
 ShaderProgram* chunkShader = nullptr;
 
+GLFWwindow* gWindow = nullptr;
+FreeCameraController* gCameraController;
+
 int main() {
 
-	GLFWwindow* window = nullptr;
 	try {
 		
 		// Initialize GLFW
-		window = initGLFW();
+		gWindow = initGLFW();
 
 		RenderingContext::init();
 		chunkShader = RenderingContext::get()->shaderCache.loadShaderProgram("chunk_shader", "chunk_vert.glsl", "chunk_frag.glsl");
@@ -60,6 +64,7 @@ int main() {
 	
 	ChunkManager::instance()->loadChunks(playerPosition);
 
+	gCameraController = new FreeCameraController(&RenderingContext::get()->camera);
 
 	RenderingContext::get()->camera.transform.translateLocal(0,160,2);
 	RenderingContext::get()->camera.transform.orient(glm::degrees(-0.0f), 0, 0);
@@ -70,7 +75,7 @@ int main() {
 	float64 delta = 0;
 	float64 currentTime = 0;
 
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(gWindow)) {
 		glfwPollEvents();
 
 		currentTime = glfwGetTime();
@@ -78,7 +83,7 @@ int main() {
 		update((float32)delta);
 		render();
 		
-		glfwSwapBuffers(window);
+		glfwSwapBuffers(gWindow);
 
 		delta = glfwGetTime() - currentTime;
 
@@ -92,6 +97,8 @@ int main() {
 			frames++;
 		}
 	}
+
+	delete gCameraController;
 
 	RenderingContext::destroy();
 	
@@ -121,11 +128,15 @@ GLFWwindow* initGLFW() {
 	
 	glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int32 width, int32 height) -> void { glViewport(0, 0, width, height); });
 
+	glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) -> void { gCameraController->onKey(key, action); });
+	glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) -> void { gCameraController->onMouseButton(button, action); });
+
 	return window;
 }
 
 void update(float32 deltaSeconds) {
 	// Update logic...
+	gCameraController->update(deltaSeconds);
 	ChunkManager::instance()->uploadQueuedChunk();
 }
 
@@ -163,4 +174,26 @@ void initTestCube() {
 	cubeModel = RenderingContext::get()->modelCache.loadModel("cube", vertices, indices);
 	cubeShader = RenderingContext::get()->shaderCache.loadShaderProgram("test_cube", "test_cube_vert.glsl", "test_cube_frag.glsl");
 	cubeTexture = RenderingContext::get()->textureCache.loadTexture2D("test_cube_texture", "test.png");
+}
+
+glm::vec2 getMouseAxis() {
+	// Poll mouse position
+	float64 mPosX;
+	float64 mPosY;
+	glfwGetCursorPos(gWindow, &mPosX, &mPosY);
+
+	static float64 lastMousePosX = mPosX;
+	static float64 lastMousePosY = mPosY;
+
+	glm::vec2 mouseAxis(0, 0);
+
+	if (mPosX != lastMousePosX || mPosY != lastMousePosY) {
+		mouseAxis.x = mPosX - lastMousePosX;
+		mouseAxis.y = lastMousePosY - mPosY;
+	}
+
+	lastMousePosX = mPosX;
+	lastMousePosY = mPosY;
+
+	return mouseAxis;
 }
